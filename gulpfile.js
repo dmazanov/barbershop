99d15899330593b09d8d90f292e1f836a547b1bd
Denis <dmazanov@mail.ru>
2017-06-04 01:10:16 +0200

    added gulp, browser-sync, Sass, readme.md

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require("gulp"),
+		browserSync = require("browser-sync").create(),
+		sass = require("gulp-sass");
+
+
+// Static Server + watching scss/html files
+gulp.task('browser-sync', ['sass'], function() {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
+	gulp.watch("*.html").on('change', browserSync.reload);
+	gulp.watch("scss/**/*.scss", ['sass']);
+	gulp.watch("js/**/*.js").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+	return gulp.src("scss/*.scss")
+		.pipe(sass())
+		.pipe(gulp.dest("css"))
+		.pipe(browserSync.stream());
+});
+
+gulp.task ('default', ['browser-sync']);
