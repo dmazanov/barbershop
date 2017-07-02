444e69d3c0d7b19a41863e31bf578384e96fef73
Denis <dmazanov@mail.ru>
2017-07-02 23:06:43 +0200

    Added task- build

--- gulpfile.js
+++ gulpfile.js
@@ -47,25 +47,7 @@ gulp.task('sass', function() {
 		.pipe(browserSync.stream());
 });
 
-gulp.task('images', function() {
-	return gulp.src("img/**/*.{png,jpg,gif}")
-		.pipe(imagemin([
-				imagemin.optipng({optimizationLevel: 3}),
-				imagemin.jpegtran({progressive: true})
-			]))
-		.pipe(gulp.dest("img"));
-});
 
-// Собираем svg sprite
-gulp.task('symbols', function() {
-	return gulp.src("img/icons/*.svg")
-		.pipe(svgmin())
-		.pipe(svgstore({
-				inlineSvg: true
-		}))
-		.pipe(rename("symbols.svg"))
-		.pipe(gulp.dest("img"));
-});
 
 gulp.task ('default', ['browser-sync']);
 
@@ -94,6 +76,50 @@ gulp.task('copy', function() {
 		.pipe(gulp.dest("build"));
 });
 
-gulp.task("build", function() {
-	run('sass', 'images', 'symbols', fn);
+
+gulp.task('style', function() {
+	return gulp.src("css/**/*.css")
+		.pipe(minify())
+		.pipe(rename("main.min.css"))
+		.pipe(gulp.dest("build/css"))
+});
+
+
+// Картинки
+gulp.task('images', function() {
+	return gulp.src("build/img/**/*.{png,jpg,gif}")
+		.pipe(imagemin([
+				imagemin.optipng({optimizationLevel: 3}),
+				imagemin.jpegtran({progressive: true})
+			]))
+		.pipe(gulp.dest("build/img"));
+});
+
+// Собираем svg sprite
+gulp.task('symbols', function() {
+	return gulp.src("build/img/icons/*.svg")
+		.pipe(svgmin())
+		.pipe(svgstore({
+				inlineSvg: true
+		}))
+		.pipe(rename("symbols.svg"))
+		.pipe(gulp.dest("build/img"));
+});
+
+// Собираем папку BUILD
+gulp.task("build", function(fn) {
+	run('clean',
+			'copy',
+			'style',
+			'images',
+			'symbols',
+			fn
+	);
+});
+
+// Проверки сборки
+gulp.task('server-build', function() {
+	browserSync.init({
+		server: "build"
+	});
 });
